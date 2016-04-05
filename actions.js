cdfde40fe3a590c3ece28c1fc4ddd55d8cdd3290
austinjkovach <kovach.a.j@gmail.com>
2016-04-05 14:29:21 -0500

    initial commit

---
+++ actions.js
@@ -0,0 +1,48 @@
+
+/*
+ * action types
+ */
+
+export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO'
+export const SET_VISIBLITY_FILTER = 'SET_VISIBLITY_FILTER'
+
+/*
+ * other constants
+ */
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/*
+ * action creators
+ */
+
+export function addTodo(text) {
+  return { type: ADD_TODO, text }
+}
+
+export function completeTodo(index){
+  return { type: COMPLETE_TODO, index }
+}
+export function setVisiblityFilter(filter){
+  return {type: SET_VISIBLITY_FILTER, filter }
+}
+
+/*
+
+  {
+    type: ADD_TODO,
+    text: 'Build my first Redux app'
+  }
+
+
+  {
+    type: COMPLETE_TODO,
+    index: 5
+  }
+
+*/
